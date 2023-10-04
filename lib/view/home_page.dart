fcd4c882ef640af56440dd7d55a809241ea4afc4
RickGutler <rickgutler42@gmail.com>
2023-10-04 20:14:01 -0300

    2 Commit
    Feito a instância de uma classe e o list view pela Mayara e feito o modal para criar um novo treino

--- lib/view/home_page.dart
+++ lib/view/home_page.dart
@@ -1,4 +1,7 @@
+import 'package:agendadetreinos/model/Exercicio.dart';
+import 'package:agendadetreinos/model/Treino.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -8,6 +11,10 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  final treinos = [
+    const Treino("Treino A", [Exercicio("Remada Cavalinho", "Costas", 4, 12)]),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -20,6 +27,89 @@ class _HomePageState extends State<HomePage> {
         ),
         backgroundColor: Colors.white,
         elevation: 0,
+      ),
+      body: ListView.builder(
+        itemCount: treinos.length,
+        itemBuilder: (context, index) {
+          return ListTile(
+                title: Text(treinos[index].nome),
+          );
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          showModalBottomSheet(
+              context: context,
+              shape: const RoundedRectangleBorder(
+                  borderRadius:
+                      BorderRadius.vertical(top: Radius.circular(24))),
+              builder: (context) {
+                return Column(
+                  children: [
+                    const SizedBox(
+                      height: 12,
+                    ),
+                    Container(
+                      width: 52,
+                      height: 10,
+                      decoration: BoxDecoration(
+                          color: Colors.grey[400],
+                          borderRadius: BorderRadius.circular(16)),
+                    ),
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(30),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.stretch,
+                        children: [
+                          Text(
+                            "CRIAR TREINO",
+                            style: TextStyle(
+                              fontFamily: GoogleFonts.inter().fontFamily,
+                              fontWeight: FontWeight.bold,
+                              fontSize: 16,
+                            ),
+                          ),
+                          const SizedBox(
+                            height: 16,
+                          ),
+                          TextFormField(
+                            decoration: const InputDecoration(
+                                labelText: "Digite um nome para o treino"),
+                          ),
+                          const SizedBox(
+                            height: 100,
+                          ),
+                          ElevatedButton(
+                              onPressed: () {},
+                              style: ButtonStyle(
+                                backgroundColor: MaterialStateProperty.all<
+                                        Color>(
+                                    const Color.fromARGB(255, 207, 206, 206)),
+                                fixedSize: MaterialStateProperty.all<Size>(
+                                    const Size(40, 40)),
+                              ),
+                              child: const Text(
+                                "Criar",
+                                style: TextStyle(
+                                    color: Colors.black,
+                                    fontSize: 16,
+                                    fontWeight: FontWeight.bold),
+                              ))
+                        ],
+                      ),
+                    )
+                  ],
+                );
+              });
+        },
+        backgroundColor: Colors.grey[200],
+        child: const Icon(
+          Icons.add,
+          color: Colors.black,
+        ),
       ),
     );
   }
